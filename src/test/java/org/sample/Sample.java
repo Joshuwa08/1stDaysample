95a064323b5c13f50b0f4e4f2ef752d53ac5cf87
Joshuwa08 <joshuwa008@gmail.com>
2022-04-19 11:35:01 +0530

    first commit

--- src/test/java/org/sample/Sample.java
+++ src/test/java/org/sample/Sample.java
@@ -7,23 +7,23 @@ public class Sample extends BaseClass {
 	
 	@BeforeClass
 	private void BeforeClass() {
-		
+		System.out.println("Before Class");
 	}
 	@AfterClass
 	private void AfterClass() {
-		
+		System.out.println("After Class");
 	}
 	@BeforeMethod
 	private void BeforeMethod() {
-		
+		System.out.println("Before Method");
 	}
 	@AfterMethod
 	private void AfterMethod() {
-		
+		System.out.println("After Method");
 	}
 	@Test
 	private void Test() {
-		
+		System.out.println("Test");
 	}
 	
 
