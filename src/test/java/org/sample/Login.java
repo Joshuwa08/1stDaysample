6dbe29e92cfe7839a3dde23969e32b032783b326
Joshuwa08 <joshuwa008@gmail.com>
2022-04-20 12:50:45 +0530

    Joshuwa 1st commit

---
+++ src/test/java/org/sample/Login.java
@@ -0,0 +1,35 @@
+package org.sample;
+
+import org.base.BaseClass;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class Login extends BaseClass{
+	public Login() {
+		PageFactory.initElements(driver, this);
+	}
+	@FindBy(id="email")
+	private WebElement username;
+	@FindBy(id="pass")
+	private WebElement password;
+	@FindBy(name="login")
+	private WebElement login;
+	public WebElement getUsername() {
+		return username;
+	}
+	public WebElement getPassword() {
+		return password;
+	}
+	public WebElement getLogin() {
+		return login;
+	}
+	
+	public void login() {
+		Login l=new Login();
+		sendkeys(l.getUsername(), "joshuwa");
+		sendkeys(l.getPassword(), "98765");
+		btnClick(l.getLogin());
+	}
+
+}
